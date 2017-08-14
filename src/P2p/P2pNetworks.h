305aa09d22c35c8378976636a935acefd028c334
coin-universe <coin-universe@users.noreply.github.com>
2017-08-14 21:24:50 +0300

    MeduzaTech
    Relize 1.0.0 fork from bytecoin 2.0.7

---
+++ src/P2p/P2pNetworks.h
@@ -0,0 +1,25 @@
+// Copyright (c) 2012-2017, The CryptoNote developers, The Bytecoin developers
+//
+// This file is part of Bytecoin.
+//
+// Bytecoin is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Lesser General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Bytecoin is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU Lesser General Public License for more details.
+//
+// You should have received a copy of the GNU Lesser General Public License
+// along with Bytecoin.  If not, see <http://www.gnu.org/licenses/>.
+
+#pragma once
+
+namespace CryptoNote
+{
+  const static boost::uuids::uuid BYTECOIN_NETWORK = { {  0x4f, 0xfe, 0xe5, 0xde, 0x62, 0x9b, 0xe6, 0x8a, 0x04, 0x53, 0xf0, 0x90, 0x44, 0x8a, 0xdb, 0x72  } };
+
+}
+
